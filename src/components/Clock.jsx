7aacd9785ec2c7a2963c3659867d95bccda3e91b
Divyansh <divyansh.rana12@gmail.com>
2020-09-19 06:03:55 +0530

    first check

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React from "react";
 
 class Clock extends React.Component {
-    formatTime(timeInSeconds) {
-        var seconds = timeInSeconds % 60;
-        var minutes = Math.floor(timeInSeconds / 60);
+  formatTime(timeInSeconds) {
+    var seconds = timeInSeconds % 60;
+    var minutes = Math.floor(timeInSeconds / 60);
 
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-
-        return minutes + ':' + seconds;
+    if (seconds < 10) {
+      seconds = "0" + seconds;
     }
 
-    render() {
-        var {timeInSeconds} = this.props;
-        //Keep the classes name. Try to inject your code and do not remove existing code
-        return (
-            <div className="clock">
-                <span className="clock-text">
-                  
-                </span>
-            </div>
-        );
+    if (minutes < 10) {
+      minutes = "0" + minutes;
     }
+
+    return minutes + ":" + seconds;
+  }
+  timer(timeInSeconds) {
+    var time = this.formatTime(timeInSeconds);
+    document.getElementsByClassName("clock-text")[0].innerHTML = time;
+  }
+  getTime(timeInSeconds) {
+    this.myInterval = setInterval(() => {
+      if (timeInSeconds < 0) {
+        return;
+      }
+      this.timer(timeInSeconds);
+      timeInSeconds = timeInSeconds - 1;
+    }, 1000);
+  }
+
+  render() {
+    var { timeInSeconds } = this.props;
+
+    //Keep the classes name. Try to inject your code and do not remove existing code
+    return (
+      <div className="clock">
+        <span className="clock-text">
+          <div>{this.getTime(timeInSeconds)}</div>
+        </span>
+      </div>
+    );
+  }
 }
-
-
 
 export default Clock;
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import Clock from './Clock';
-import CountdownForm from './CountdownForm';
+import React from "react";
+import Clock from "./Clock";
+import CountdownForm from "./CountdownForm";
 
 class Countdown extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  changeCount = (value) => {
+    this.setState({ count: value });
+  };
 
-    render() {
-
-        return (
-            <div>
-                <Clock timeInSeconds={count}/>
-                <CountdownForm onSetCountdownTime={}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Clock timeInSeconds={this.state.count} />
+        <CountdownForm onSetCountdownTime={(val) => this.changeCount(val)} />
+      </div>
+    );
+  }
 }
 
 export default Countdown;
